3c9ba4010d772c5d39c9e43761f3d57061558a14
FouadHijazi <fouad.o.hijazi@gmail.com>
2022-09-10 18:19:14 +0300

    import schemas

--- models/db.js
+++ models/db.js
@@ -4,6 +4,9 @@ const mongoose = require("mongoose");
 mongoose.connect(process.env.DB_URI).then(
   () => {
     console.log("DB Ready To Use");
+    require("./clientSchema")
+    require("./userSchema")
+    require("./visitSchema")
   },
   (err) => {
     console.log(err);
