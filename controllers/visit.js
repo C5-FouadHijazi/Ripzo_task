bb9afd96af0d026a4a8bdd8971b6017653076783
FouadHijazi <Fouad.o.hijazi@gmail.com>
2022-09-13 13:54:58 +0300

    implement part 1

--- controllers/visit.js
+++ controllers/visit.js
@@ -1,15 +1,87 @@
 const visitModel = require("../models/visitSchema");
 
-
 const getLessVisitors = async (req, res) => {
   const { from, to, day } = req.query;
-  const visits = await visitModel.find({ time: { $lte: +to , $gte: +from } })
-  .populate("client")
+  const daysMap = {
+    sunday: 1,
+    monday: 2,
+    tuesday: 3,
+    wednesday: 4,
+    thursday: 5,
+    friday: 6,
+    saturday: 7,
+  };
+  const dayNumber = daysMap[day];
+
+  const visits = await visitModel.aggregate([
+    { $match: { time: { $lte: +to, $gte: +from } } },
+    {
+      $project: {
+        date: {
+          $toDate: "$time",
+        },
+        client: 1,
+      },
+    },
+    {
+      $project: {
+        day: {
+          $dayOfWeek: "$date",
+        },
+        client: 1,
+      },
+    },
+    {
+      $match: {
+        day: dayNumber,
+      },
+    },
+    {
+      $group: {
+        _id: {
+          client: "$client",
+        },
+        count: {
+          $sum: 1,
+        },
+      },
+    },
+    {
+      $sort: {
+        count: 1,
+      },
+    },
+    {
+      $limit: 10,
+    },
+    {
+      $lookup: {
+        from: "clients",
+        localField: "_id.client",
+        foreignField: "_id",
+        as: "client",
+      },
+    },
+    {
+      $unwind: {
+        path: "$client",
+        preserveNullAndEmptyArrays: false,
+      },
+    },
+    {
+      $project: {
+        _id: 0,
+        client: 1,
+      },
+    },
+  ]);
   res.json({
     success: true,
-    visits 
+    visits,
   });
-}
+};
+
+
 
 module.exports = {
   getLessVisitors,
